20db33c5bb691684bd452dd30faab81ff0d391d4
Astyan <astyan@omnos.fr>
2016-09-01 22:11:42 +0200

    adding science

---
+++ skepticalsciencewebsite/sciences/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from skepticalsciencewebsite.sciences.models import Science
+
+# Register your models here.
+admin.site.register(Science)
---
+++ skepticalsciencewebsite/sciences/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Science(models.Model):
+    name = models.CharField(verbose_name = "Name",
+                            max_length = 255,
+                            unique=True)
+    description = models.CharField(verbose_name = "Description",
+                                   max_length = 2048,
+                                   unique=True)
+    subscience = models.ManyToManyField("self")
