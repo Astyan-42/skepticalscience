e2ba63f1aab0c092ed7036414ab257e4a72d2ff9
Astyan <astyan@omnos.fr>
2016-09-03 15:42:14 +0200

    delete registration app because useless with hmac

--- skepticalsciencewebsite/skepticalsciencewebsite/settings.py
+++ skepticalsciencewebsite/skepticalsciencewebsite/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    # 'registration',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
