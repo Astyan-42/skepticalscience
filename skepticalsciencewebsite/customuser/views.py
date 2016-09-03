b44a449544aba103ae1f37048720b4646655ef95
Astyan <astyan@omnos.fr>
2016-09-03 19:00:42 +0200

    starting the profile view

--- skepticalsciencewebsite/customuser/views.py
+++ skepticalsciencewebsite/customuser/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.views.generic import UpdateView, DetailView
+from customuser.models import User
 # Create your views here.
+
+
+class UserDetailView(DetailView):
+    model = User
+    # template_name = 'customuser/detail_user.html'
+
+class UserUpdateView(UpdateView):
+    model = User
+    # form_class = UserUpdateForm
+    # template_name = 'customuser/update_user.html'
