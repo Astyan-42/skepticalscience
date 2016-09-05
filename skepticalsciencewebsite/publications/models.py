9fb3c3c084ed06fa2c9772db15ad9c406c630a52
Astyan <astyan@omnos.fr>
2016-09-05 23:13:43 +0200

    start the model depending of the publication

--- skepticalsciencewebsite/publications/models.py
+++ skepticalsciencewebsite/publications/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.core.validators import RegexValidator
+from sciences.models import Science
+from customuser.models import User,MinMaxFloat
 # Create your models here.
+
+
+class Publication(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
+
+
+class Reviewer(models.Model):
+    pass
