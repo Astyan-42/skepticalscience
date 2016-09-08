9e4b02da28c0e94537635c25ea38b334c9b8178f
Astyan <astyan@omnos.fr>
2016-09-08 19:32:35 +0200

    science sorted by name in _science_choices

--- skepticalsciencewebsite/sciences/forms.py
+++ skepticalsciencewebsite/sciences/forms.py
@@ -4,6 +4,7 @@ from sciences.models import Science
 
 def _science_choices(queryset, prefix=''):
     choices = []
+    queryset = sorted(queryset, key=lambda science: science.name)
     for science in queryset:
         choices.append((science.id, prefix+science.__str__()))
         res = _science_choices(science.sub_science.all(), prefix=prefix+'-')
