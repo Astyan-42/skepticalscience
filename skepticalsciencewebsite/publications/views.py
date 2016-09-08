1c7d08df6ffb76f1c406786fc11fc699c711f864
Astyan <astyan@omnos.fr>
2016-09-08 13:00:23 +0200

    user added automaticaly

--- skepticalsciencewebsite/publications/views.py
+++ skepticalsciencewebsite/publications/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.http import HttpResponseRedirect
 from publications.models import Publication
 from publications.forms import PublicationCreateForm
 # Create your views here.
 
 
+@method_decorator(login_required, name='dispatch')
 class PublicationCreate(CreateView):
     model = Publication
     form_class = PublicationCreateForm
     success_url = reverse_lazy("index")
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.editor = self.request.user
+        obj.save()
+        return HttpResponseRedirect(self.success_url)
