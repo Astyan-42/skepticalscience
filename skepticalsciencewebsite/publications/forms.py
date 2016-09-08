b3eb52037d9e525937976c30abd67d9aeccde6fb
Astyan <astyan@omnos.fr>
2016-09-08 18:40:00 +0200

    science better choice done (just need to move the function and do it in a proper way

--- skepticalsciencewebsite/publications/forms.py
+++ skepticalsciencewebsite/publications/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 from django_select2.forms import Select2MultipleWidget
 from publications.models import Publication
+from sciences.models import Science
+
+
+def science_choices(queryset, prefix=''):
+    choices = []
+    for science in queryset:
+        choices.append((science.id, prefix+science.__str__()))
+        res = science_choices(science.sub_science.all(), prefix=prefix+'-')
+        choices = choices + res
+    return choices
 
 
 class PublicationCreateForm(forms.ModelForm):
@@ -12,3 +22,12 @@ class PublicationCreateForm(forms.ModelForm):
         fields = ["title", "resume_creation", "pdf_creation", "source_creation", "sciences", "licence"]
         widgets = {'sciences': Select2MultipleWidget,
                    'resume_creation': forms.Textarea(attrs={'class': 'form-control'})}
+
+    def __init__(self, *args, **kwargs):
+        super(PublicationCreateForm, self).__init__(*args, **kwargs)
+        sciences = Science.objects.filter(primary_science=True)
+        choices = science_choices(sciences)
+        self.fields["sciences"].choices = choices
+
+
+
