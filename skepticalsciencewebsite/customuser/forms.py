78ee7253f1c5873d835c1bf3f18f55998a862c41
Astyan <astyan@omnos.fr>
2016-09-13 16:32:13 +0200

    crispy form on user

--- skepticalsciencewebsite/customuser/forms.py
+++ skepticalsciencewebsite/customuser/forms.py
@@ -1,8 +1,10 @@
-from django import forms
 from registration.forms import RegistrationForm
+from django.utils.translation import ugettext_lazy as _
 from customuser.models import User
 from django_select2.forms import Select2MultipleWidget
 from sciences.forms import ScienceModelForm
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 
 class CustomUserForm(RegistrationForm):
@@ -18,6 +20,13 @@ class CustomUserUpdateForm(ScienceModelForm):
     """
     create an user form with restricted field (the field could be directly in the view, used the for
     """
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserUpdateForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_id = 'id-userupdateForm'
+        self.helper.add_input(Submit('submit', _('Submit')))
+
     class Meta:
         model = User
         fields = ["email", "first_name", "middle_name", "last_name", "phd", "country", "workplace", "description",
