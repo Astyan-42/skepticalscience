1ea6fa18ec1c128154848b1157f7f99eb6efe4f6
Astyan <astyan@omnos.fr>
2016-09-16 21:14:37 +0200

    changing creation form to add first author with name

--- skepticalsciencewebsite/publications/forms.py
+++ skepticalsciencewebsite/publications/forms.py
@@ -1,16 +1,26 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 from django_select2.forms import Select2MultipleWidget
-from publications.models import Publication, Comment
-from sciences.forms import ScienceModelForm
+from django.db.models import Q
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
+from publications.models import Publication, Comment
+from sciences.forms import ScienceModelForm
+from customuser.models import User
+
+
+class UserModelChoiceField(forms.ModelChoiceField):
+
+    def label_from_instance(self, obj):
+        return obj.get_full_name()
 
 
 class PublicationCreateForm(ScienceModelForm):
     """
     create an publication form with restricted field
     """
+    first_author=UserModelChoiceField(queryset=User.objects.filter(~Q(first_name="") & ~Q(last_name="")))
+
     def __init__(self, *args, **kwargs):
         super(PublicationCreateForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
@@ -21,7 +31,7 @@ class PublicationCreateForm(ScienceModelForm):
         model = Publication
         fields = ["title", "resume", "pdf_creation", "source_creation", "first_author", "sciences", "licence"]
         widgets = {'sciences': Select2MultipleWidget,
-                   'resume_creation': forms.Textarea(attrs={'class': 'form-control'})}
+                   'resume': forms.Textarea()}
 
 
 class CommentForm(forms.ModelForm):
