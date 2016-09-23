3a0a6126851eea42ecaabb27dde78e537116c283
Astyan <astyan@omnos.fr>
2016-09-23 04:29:03 +0200

    cron squeleton bigger

--- skepticalsciencewebsite/publications/cron.py
+++ skepticalsciencewebsite/publications/cron.py
@@ -1,4 +1,5 @@
 from django_cron import CronJobBase, Schedule
+from publications.models import Publication
 
 
 class PublicationUpdateCronJob(CronJobBase):
@@ -6,6 +7,26 @@ class PublicationUpdateCronJob(CronJobBase):
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'publications.publication_update_cron_job'    # a unique code
+
+    def update_adding_peer_to_peer_review(self, publications):
+        # check if there is enough reviewer (and 1 week) then change the status
+        pass
+
+    def update_peer_review_to_correction(self, publications):
+        # check the time period is over 3 week ?then change the status and update the users
+        pass
+
+    def update_correction_to_validation(self, publications):
+        # check if the period is over 3 week ? then change the status
+        pass
+
+    def update_validation_to_evaluation(self, publications):
+        # check if the period is over 1 week ? then change the status
+        pass
+
+    def update_evaluation_to_published(self, publications):
+        # check if the period is over 1 week ? then change the status
+        pass
 
     def do(self):
         pass    # do your thing here
