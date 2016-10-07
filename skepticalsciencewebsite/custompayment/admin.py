c07627ca4a02f52d0ab86abe964b737f0bc3dd7b
Astyan <astyan@omnos.fr>
2016-10-07 22:09:06 +0200

    little modification

--- skepticalsciencewebsite/custompayment/admin.py
+++ skepticalsciencewebsite/custompayment/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from  custompayment.models import Payment
 # Register your models here.
+
+admin.site.register(Payment)
--- skepticalsciencewebsite/skepticalsciencewebsite/settings.py
+++ skepticalsciencewebsite/skepticalsciencewebsite/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'customuser',
     'publications',
     'faq',
+    'custompayment',
 ]
 
 MIDDLEWARE = [
