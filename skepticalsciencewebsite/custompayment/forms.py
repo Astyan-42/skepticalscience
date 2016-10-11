58fee39160227ad2fa9719fbd01469410a2911af
Astyan <astyan@omnos.fr>
2016-10-12 00:52:42 +0200

    little deletion

--- skepticalsciencewebsite/custompayment/forms.py
+++ skepticalsciencewebsite/custompayment/forms.py
@@ -1,8 +1,5 @@
 from django import forms
 from django.conf import settings
-from payments import get_payment_model
-
-Payment = get_payment_model()
 
 
 class PaymentMethodsForm(forms.Form):
--- skepticalsciencewebsite/custompayment/urls.py
+++ skepticalsciencewebsite/custompayment/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     url(r'^(?P<order>[-\w]+)/$', details, name='details'),
     url(r'^(?P<order>[-\w]+)/payment/$', payment_choice, name='payment'),
     url(r'^(?P<order>[-\w]+)/payment/(?P<variant>[-\w]+)/$', start_payment, name='payment')
-    #url(r'^%s/cancel-payment/$' % (TOKEN_PATTERN,), views.cancel_payment, name='cancel-payment')
 ]
