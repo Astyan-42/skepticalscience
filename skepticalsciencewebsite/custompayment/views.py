4877f7bc4c028f4908aec7a3e4bb40a394256afc
Astyan <astyan@omnos.fr>
2016-10-11 23:51:04 +0200

    improving payment app

--- skepticalsciencewebsite/custompayment/views.py
+++ skepticalsciencewebsite/custompayment/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404, redirect
 from django.template.response import TemplateResponse
 from payments import get_payment_model, RedirectNeeded
-from custompayment.models import Order
+from custompayment.models import Order, Payment
+from custompayment.forms import PaymentMethodsForm
 
 
 def details(request, order):
@@ -12,10 +13,14 @@ def details(request, order):
 
 
 def payment(request, order):
-    orders = Order.objects.prefetch_related('groups__items')
-    order = get_object_or_404(orders, order_id=order)
-    return TemplateResponse(request, 'custompayment/details.html',
-                            {'order': order})
+    order = get_object_or_404(Order, order_id=order)
+    payments = order.payments.all()
+    form_data = request.POST or None
+    payment_form = PaymentMethodsForm(form_data)
+    return TemplateResponse(request, 'custompayment/payment.html',
+                            {'order': order,
+                             'payment_form': payment_form,
+                             'payments': payments})
 
 
 def start_payment(request, order):
