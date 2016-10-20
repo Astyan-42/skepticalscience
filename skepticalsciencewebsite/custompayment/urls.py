b4c175438d7a29ab1264dc5ea1e9f4a8bb6b07ac
Astyan <astyan@omnos.fr>
2016-10-20 09:52:37 +0200

    adding create_order to the url

--- skepticalsciencewebsite/custompayment/urls.py
+++ skepticalsciencewebsite/custompayment/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from custompayment.views import (payment_choice, start_payment, BillingAddressUpdate,
+from custompayment.views import (create_order, payment_choice, start_payment, BillingAddressUpdate,
                                  OrderDetailView, OrderOwnedTableView)
 
 urlpatterns = [
     url(r'^orders/', OrderOwnedTableView.as_view(), name='list_order'),
+    url(r'^new_order/(?P<name>[-\w]+)/?(P<iku>\d)/$', create_order, name='create_order'),
     url(r'^(?P<token>[-\w]+)/$', OrderDetailView.as_view(), name='detail_order'),
     url(r'^(?P<token>[-\w]+)/address/$', BillingAddressUpdate.as_view(), name='address'),
     url(r'^(?P<token>[-\w]+)/payment/$', payment_choice, name='payment'),
