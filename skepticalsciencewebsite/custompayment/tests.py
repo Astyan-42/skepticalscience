5f8c764d3e2f1e23b5de05ed9f3936a91d5861c4
Astyan <astyan@omnos.fr>
2016-10-22 16:54:52 +0200

    test to tries to find why the receiver is not used

--- skepticalsciencewebsite/custompayment/tests.py
+++ skepticalsciencewebsite/custompayment/tests.py
@@ -1,7 +1,43 @@
 from django.test import TestCase
 from decimal import Decimal
-from payments import get_payment_model
 from django.shortcuts import redirect
-from custompayment.views import payment_details
+from customuser.models import User
+from custompayment.models import Payment, Order, Item
 # Create your tests here.
+
+
+class PaymentStatusTestCase(TestCase):
+
+    def setUp(self):
+        item = Item(name="publication", sku=1)
+        item.save()
+        user = User.objects.create(username="testuser", password="azerty123", phd=True, first_name="Jesus",
+                                    middle_name="Our Savior", last_name="Raptor", email="testpub1@test.com")
+        order = Order(item=item, user=user)
+        order.save()
+        defaults = {'total': Decimal(120),
+                    'tax': Decimal(20),
+                    'currency': 'USD',
+                    'delivery': Decimal(10),
+                    'billing_first_name': 'Sherlock',
+                    'billing_last_name': 'Holmes',
+                    'billing_address_1': '221B Baker Street',
+                    'billing_address_2': '',
+                    'billing_city': 'London',
+                    'billing_postcode': 'NW1 6XE',
+                    'billing_country_code': 'UK',
+                    'description': 'Order %(order_number)s' % {'order_number': order},
+                    'billing_country_area': 'Greater London',
+                    'customer_ip_address': '127.0.0.1'}
+        self.payment, res = Payment.objects.get_or_create(variant="dummy", status='waiting', order=order, defaults=defaults)
+        self.payment.save()
+
+    def test_change_status(self):
+        # unable to test status change or status_change don't work
+        self.payment.change_status('preauth')
+        order = Order.objects.get(pk=self.payment.order.pk)
+        print(order.status)
+        self.payment.change_status('confirmed')
+
+
 
--- skepticalsciencewebsite/custompayment/utils.py
+++ skepticalsciencewebsite/custompayment/utils.py
@@ -1,11 +1,17 @@
 from django.dispatch import receiver
 from payments.signals import status_changed
+from custompayment.constants import FULLY_PAID
 
 
 @receiver(status_changed)
-def order_status_change(sender, instance, **kwargs):
+def payment_status_change(sender, instance, **kwargs):
     #instance is payment
+    print("test")
     order = instance.order
-    # if order.is_fully_paid():
-    #     order.change_status('fully-paid')
-    #     instance.send_confirmation_email()
+    order.status = FULLY_PAID
+    order.save()
+    # if instance.status == 'confirmed':
+    #     order.status = FULLY_PAID
+    #     order.save()
+        # need to change the publication status or put the user in the group of scientist
+        # send a confirmation email ?
