90bc1bce93a47d274a29b49b6da41c8d110e3997
Astyan <astyan@omnos.fr>
2016-12-28 07:54:51 +0100

    correcting the print of the order list

--- skepticalsciencewebsite/custompayment/filters.py
+++ skepticalsciencewebsite/custompayment/filters.py
@@ -6,4 +6,4 @@ class OrderFilter(django_filters.FilterSet):
 
     class Meta:
         model = Order
-        fields = ["status", "creation_date"]
+        fields = ["status", "creation_date", "user"]
