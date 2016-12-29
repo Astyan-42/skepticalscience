d1770dbf6ab3e479b04548a05dc44c531c495f56
Astyan <astyan@omnos.fr>
2016-12-29 19:24:12 +0100

    better login redirection

--- skepticalsciencewebsite/skepticalsciencewebsite/settings.py
+++ skepticalsciencewebsite/skepticalsciencewebsite/settings.py
@@ -179,7 +179,7 @@ REGISTRATION_OPEN = True                # If True, users can register
 ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
 REGISTRATION_SALT = "github"
 # REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
-LOGIN_REDIRECT_URL = '/accounts/login/'  # The page you want users to arrive at after they successful log in
+LOGIN_REDIRECT_URL = '/'  # The page you want users to arrive at after they successful log in
 LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
 
 # Email
