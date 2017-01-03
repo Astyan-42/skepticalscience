f16984d32737f9f86999a00c9e10b27fcc4ce92d
Astyan <astyan@omnos.fr>
2017-01-03 20:51:46 +0100

    using mock with faq models

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     install_requires=['django', 'django-bootstrap3', 'django-registration', 'django-select2', 'django-sendfile',
                       'django_tables2', 'django-filter', 'django-crispy-forms', 'django-simple-history', 'django_cron',
                       'django-analytical', 'django-payments', 'django-countries', 'django-cookie-law', 'reportlab',
-                      'PyInvoice'],
+                      'PyInvoice', 'coverage', 'mock'],
 
     # List additional groups of dependencies here (e.g. development dependencies).
     # You can install these using the following syntax, for example:
--- skepticalsciencewebsite/faq/tests/test_models.py
+++ skepticalsciencewebsite/faq/tests/test_models.py
@@ -1,5 +1,20 @@
+import mock
 from django.test import TestCase
-from faq import models
+from faq.models import Topic, QandA
 
-#nohing to do like everything comes from django
 
+class TopicTestCase(TestCase):
+
+    def test__str__(self):
+        topic = mock.Mock(spec=Topic)
+        topic.name = "test name"
+        self.assertEqual(Topic.__str__(topic), "test name")
+
+
+class QandATestCase(TestCase):
+
+    def test__str__(self):
+        qanda = mock.Mock(spec=QandA)
+        qanda.question = "test question"
+        qanda.answer = "test answer"
+        self.assertEqual(QandA.__str__(qanda), "test question")
