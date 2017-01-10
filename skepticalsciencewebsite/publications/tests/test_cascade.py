f1fb1dae451722476c16ac3529d6eca40e92cd54
Astyan <astyan@omnos.fr>
2017-01-11 00:09:24 +0100

    improve the test for cascade

--- skepticalsciencewebsite/publications/tests/test_cascade.py
+++ skepticalsciencewebsite/publications/tests/test_cascade.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.utils import timezone
+from django.contrib.auth import get_user_model
 from publications.models import Publication, Licence, Comment, CommentReview, Reviewer, EstimatedImpactFactor
 from publications.cascade import (update_comment_validation, update_comment_correction, update_user_skeptic_score,
                                   update_publication_score_peer_review_to_correction,
@@ -8,42 +9,21 @@ from publications.cascade import (update_comment_validation, update_comment_corr
                                   update_reviewers_score_validation_to_evaluation,
                                   update_median_impact_factor_publication, update_mean_impact_factor_users)
 from publications.constants import *
-from customuser.models import User
-
-# Create your tests here.
-
-
-class PublicationTestCase(TestCase):
-
-    def setUp(self):
-        """
-        Store an user
-        """
-        jesus = User.objects.create(username="testuser", password="azerty123", phd=True, first_name="Jesus",
-                                    middle_name="Our Savior", last_name="Raptor", email="testpub1@tests.com")
-        fsm = User.objects.create(username="testuser2", password="azerty123", phd=True, first_name="Flying",
-                                  middle_name="Spaghetti", last_name="Monster", email="testpub2@tests.com")
-        l = Licence.objects.create(short_name="lol", full_name="lol", url="http://google.com")
-        Publication.objects.create(editor=jesus, title="lol", first_author=jesus, last_author=fsm, resume="lol",
-                                   licence=l)
-
-    def test_get_all_authors(self):
-        publi = Publication.objects.get(title="lol")
-        self.assertEqual(2, len(publi.get_all_authors))
 
 
 class CascadeTestCase(TestCase):
 
     def setUp(self):
-        self.jesus = User.objects.create(username="testuser", password="azerty123", phd=True, first_name="Jesus",
-                                    middle_name="Our Savior", last_name="Raptor", email="testcasc1@tests.com")
-        self.fsm = User.objects.create(username="testuser2", password="azerty123", phd=True, first_name="Flying",
-                                       middle_name="Spaghetti", last_name="Monster", email="testcasc2@tests.com")
-        self.rael = User.objects.create(username="testuser3", password="azerty123", phd=True, first_name="Rael",
-                                        last_name="ET", email="testcasc3@tests.com")
-        self.ipu = User.objects.create(username="testuser4", password="azerty123", phd=True, first_name="Invisible",
-                                       middle_name="Pink", last_name="Unicorn", email="testcasc4@tests.com")
-        self.ft = User.objects.create(username="testuser5", password="azerty123", phd=True, first_name="Flying",
+        self.User = get_user_model()
+        self.jesus = self.User.objects.create(username="testuser", password="azerty123", phd=True, first_name="Jesus",
+                                              middle_name="Our Savior", last_name="Raptor", email="testcasc1@tests.com")
+        self.fsm = self.User.objects.create(username="testuser2", password="azerty123", phd=True, first_name="Flying",
+                                            middle_name="Spaghetti", last_name="Monster", email="testcasc2@tests.com")
+        self.rael = self.User.objects.create(username="testuser3", password="azerty123", phd=True, first_name="Rael",
+                                             last_name="ET", email="testcasc3@tests.com")
+        self.ipu = self.User.objects.create(username="testuser4", password="azerty123", phd=True, first_name="Invisible",
+                                            middle_name="Pink", last_name="Unicorn", email="testcasc4@tests.com")
+        self.ft = self.User.objects.create(username="testuser5", password="azerty123", phd=True, first_name="Flying",
                                       last_name="Teapot", email="testcasc5@tests.com")
         l = Licence.objects.create(short_name="lol", full_name="lol", url="http://google.com")
         self.publication = Publication.objects.create(editor=self.jesus, title="lol", first_author=self.jesus,
@@ -146,24 +126,24 @@ class CascadeTestCase(TestCase):
 
     def test_update_user_skeptic_score_not_yet(self):
         self.assertEqual(False, update_user_skeptic_score(self.comment.pk))
-        self.assertEqual(0, User.objects.get_by_natural_key(self.fsm).valid_bias_found)
-        self.assertEqual(0, User.objects.get_by_natural_key(self.fsm).invalid_bias_found)
+        self.assertEqual(0, self.User.objects.get_by_natural_key(self.fsm).valid_bias_found)
+        self.assertEqual(0, self.User.objects.get_by_natural_key(self.fsm).invalid_bias_found)
 
     def test_update_user_skeptic_score_dismiss(self):
         self.comment.validated = DISMISS
         self.comment.save()
         self.assertEqual(True, update_user_skeptic_score(self.comment.pk))
-        self.assertEqual(0, User.objects.get_by_natural_key(self.fsm).valid_bias_found)
-        self.assertEqual(1, User.objects.get_by_natural_key(self.fsm).invalid_bias_found)
-        self.assertEqual(0, User.objects.get_by_natural_key(self.fsm).skeptic_score)
+        self.assertEqual(0, self.User.objects.get_by_natural_key(self.fsm).valid_bias_found)
+        self.assertEqual(1, self.User.objects.get_by_natural_key(self.fsm).invalid_bias_found)
+        self.assertEqual(0, self.User.objects.get_by_natural_key(self.fsm).skeptic_score)
 
     def test_update_user_skeptic_score_validated(self):
         self.comment.validated = VALIDATE
         self.comment.save()
         self.assertEqual(True, update_user_skeptic_score(self.comment.pk))
-        self.assertEqual(1, User.objects.get_by_natural_key(self.fsm).valid_bias_found)
-        self.assertEqual(0, User.objects.get_by_natural_key(self.fsm).invalid_bias_found)
-        self.assertEqual(10., User.objects.get_by_natural_key(self.fsm).skeptic_score)
+        self.assertEqual(1, self.User.objects.get_by_natural_key(self.fsm).valid_bias_found)
+        self.assertEqual(0, self.User.objects.get_by_natural_key(self.fsm).invalid_bias_found)
+        self.assertEqual(10., self.User.objects.get_by_natural_key(self.fsm).skeptic_score)
 
     def test_update_publication_score_peer_review_to_correction_bad_status(self):
         self.assertEqual(False, update_publication_score_peer_review_to_correction(self.publication.pk))
@@ -182,6 +162,14 @@ class CascadeTestCase(TestCase):
         self.comment.save()
         self.assertEqual(True,  update_publication_score_peer_review_to_correction(self.publication.pk))
         self.assertEqual(8., Publication.objects.get(pk=self.publication.pk).publication_score)
+        self.comment.seriousness = MINOR
+        self.comment.save()
+        self.assertEqual(True,  update_publication_score_peer_review_to_correction(self.publication.pk))
+        self.assertEqual(9., Publication.objects.get(pk=self.publication.pk).publication_score)
+        self.comment.seriousness = CRITICAL
+        self.comment.save()
+        self.assertEqual(True,  update_publication_score_peer_review_to_correction(self.publication.pk))
+        self.assertEqual(7., Publication.objects.get(pk=self.publication.pk).publication_score)
 
     def test_update_publication_score_validation_to_evaluation_bad_status(self):
         self.assertEqual(False, update_publication_score_validation_to_evaluation(self.publication.pk))
@@ -203,10 +191,22 @@ class CascadeTestCase(TestCase):
         self.comment.save()
         self.assertEqual(True, update_publication_score_validation_to_evaluation(self.publication.pk))
         self.assertEqual(9., Publication.objects.get(pk=self.publication.pk).publication_score)
+        self.publication.publication_score = 8.
+        self.publication.save()
+        self.comment.seriousness = MINOR
+        self.comment.save()
+        self.assertEqual(True, update_publication_score_validation_to_evaluation(self.publication.pk))
+        self.assertEqual(8.5, Publication.objects.get(pk=self.publication.pk).publication_score)
+        self.publication.publication_score = 8.
+        self.publication.save()
+        self.comment.seriousness = CRITICAL
+        self.comment.save()
+        self.assertEqual(True, update_publication_score_validation_to_evaluation(self.publication.pk))
+        self.assertEqual(10., Publication.objects.get(pk=self.publication.pk).publication_score)
 
     def test_add_publication_to_user(self):
         self.assertEqual(True, add_publication_to_user(self.publication.pk))
-        self.assertEqual(1, User.objects.get_by_natural_key(self.jesus).nb_publication)
+        self.assertEqual(1, self.User.objects.get_by_natural_key(self.jesus).nb_publication)
 
     def test_update_user_mean_publication_score(self):
         self.publication.publication_score = 4.2
@@ -214,28 +214,28 @@ class CascadeTestCase(TestCase):
         self.jesus.nb_publication = 1.
         self.jesus.save()
         self.assertEqual(True, update_user_mean_publication_score(self.publication.pk))
-        self.assertEqual(4.2, User.objects.get_by_natural_key(self.jesus).mean_publication_score)
+        self.assertEqual(4.2, self.User.objects.get_by_natural_key(self.jesus).mean_publication_score)
 
     def test_update_reviewers_score_peer_review_to_correction(self):
         self.comment.validated = VALIDATE
         self.comment.seriousness = MAJOR
         self.comment.save()
-        CommentReview.objects.create(reviewer=self.rrael, comment=self.comment, seriousness=MAJOR,
+        CommentReview.objects.create(reviewer=self.rrael, comment=self.comment, seriousness=MINOR,
                                      reason_validation="lol", valid=True)
         CommentReview.objects.create(reviewer=self.ripu, comment=self.comment, seriousness=MAJOR,
                                      reason_validation="lol", valid=True)
         CommentReview.objects.create(reviewer=self.rft, comment=self.comment, seriousness=CRITICAL,
                                      reason_validation="lol", valid=True)
         self.assertEqual(True, update_reviewers_score_peer_review_to_correction(self.publication.pk))
-        rael = User.objects.get_by_natural_key(self.rael)
+        rael = self.User.objects.get_by_natural_key(self.rael)
         self.assertEqual(1, rael.comments_evaluated)
         self.assertEqual(0, rael.comments_not_evaluated)
         self.assertEqual(1.0, rael.reviewer_score)
-        ipu = User.objects.get_by_natural_key(self.ipu)
+        ipu = self.User.objects.get_by_natural_key(self.ipu)
         self.assertEqual(1, ipu.comments_evaluated)
         self.assertEqual(0, ipu.comments_not_evaluated)
         self.assertEqual(1.0, ipu.reviewer_score)
-        fsm = User.objects.get_by_natural_key(self.fsm)
+        fsm = self.User.objects.get_by_natural_key(self.fsm)
         self.assertEqual(0, fsm.comments_evaluated)
         self.assertEqual(1, fsm.comments_not_evaluated)
         self.assertEqual(0.0, fsm.reviewer_score)
@@ -256,15 +256,15 @@ class CascadeTestCase(TestCase):
                                      reason_validation="lol", valid=True, corrected_date=timezone.now(),
                                      corrected=True, reason_correction="lol")
         self.assertEqual(True, update_reviewers_score_validation_to_evaluation(self.publication.pk))
-        rael = User.objects.get_by_natural_key(self.rael)
+        rael = self.User.objects.get_by_natural_key(self.rael)
         self.assertEqual(1, rael.comments_evaluated)
         self.assertEqual(0, rael.comments_not_evaluated)
         self.assertEqual(1.0, rael.reviewer_score)
-        ipu = User.objects.get_by_natural_key(self.ipu)
+        ipu = self.User.objects.get_by_natural_key(self.ipu)
         self.assertEqual(1, ipu.comments_evaluated)
         self.assertEqual(0, ipu.comments_not_evaluated)
         self.assertEqual(1.0, ipu.reviewer_score)
-        fsm = User.objects.get_by_natural_key(self.fsm)
+        fsm = self.User.objects.get_by_natural_key(self.fsm)
         self.assertEqual(0, fsm.comments_evaluated)
         self.assertEqual(1, fsm.comments_not_evaluated)
         self.assertEqual(0.0, fsm.reviewer_score)
@@ -289,5 +289,5 @@ class CascadeTestCase(TestCase):
         self.jesus.nb_publication = 1
         self.jesus.save()
         self.assertEqual(True, update_mean_impact_factor_users(self.publication.pk))
-        jesus = User.objects.get_by_natural_key(self.jesus)
+        jesus = self.User.objects.get_by_natural_key(self.jesus)
         self.assertEqual(23., jesus.mean_impact_factor)
